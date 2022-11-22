257a377909e201f7e5e0c3c73387420565a92f6a
李琦 <qi.li@huilianyi.com>
2022-11-22 20:14:45 +0800

    页面样式结构调整

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -28,15 +28,15 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
-        getItem('Tom', '3'),
-        getItem('Bill', '4'),
-        getItem('Alex', '5'),
+    getItem('菜单1', '1', <PieChartOutlined />),
+    getItem('菜单2', '2', <DesktopOutlined />),
+    getItem('用户菜单', 'sub1', <UserOutlined />, [
+        getItem('小李', '3'),
+        getItem('小黄', '4'),
+        getItem('小张', '5'),
     ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
+    getItem('菜单3', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('菜单4', '9', <FileOutlined />),
 ];
 
 const View: React.FC = () => {
@@ -49,17 +49,18 @@ const View: React.FC = () => {
                 <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
             </Sider>
             <Layout className="site-layout">
-                <Header className="site-layout-background" style={{ padding: 0 }} />
-                <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>User</Breadcrumb.Item>
-                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                <Header className="site-layout-background" style={{ paddingLeft: 24 }} >
+                    <Breadcrumb style={{ lineHeight: "64px" }} >
+                        <Breadcrumb.Item>用户</Breadcrumb.Item>
+                        <Breadcrumb.Item>小李</Breadcrumb.Item>
                     </Breadcrumb>
-                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                        Bill is a cat.
+                </Header>
+                <Content style={{ margin: '16px 16px 0 16px' }} className="layout-height-container">
+                    <div className="site-layout-background layout-height-container" style={{ padding: 24, minHeight: 360 }}>
+                        这是一个大的内容区域
                     </div>
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                <Footer style={{ textAlign: 'center', padding: 0, lineHeight: "48px" }}>设计者：小李</Footer>
             </Layout>
         </Layout>
     );
