93f77724abccd3d4af4c6b4918739d5aedeba087
李琦 <qi.li@huilianyi.com>
2022-11-27 16:37:22 +0800

    获取登录信息

--- src/components/menu/main-menu.tsx
+++ src/components/menu/main-menu.tsx
@@ -5,21 +5,21 @@ import {useNavigate, useLocation} from "react-router-dom";
 
 type MenuItem = Required<MenuProps>['items'][number];
 
-const items:MenuItem[] = [
+const items: MenuItem[] = [
     {
         label: "菜单1",
         key: "/page1",
-        icon:  <PieChartOutlined />
+        icon: <PieChartOutlined/>
     },
     {
         label: "菜单2",
         key: "/page2",
-        icon:  <DesktopOutlined />
+        icon: <DesktopOutlined/>
     },
     {
         label: "菜单3",
         key: "page3",
-        icon:  <UserOutlined />,
+        icon: <UserOutlined/>,
         children: [
             {
                 label: "小李",
@@ -34,7 +34,7 @@ const items:MenuItem[] = [
     {
         label: "菜单4",
         key: "page4",
-        icon:  <TeamOutlined />,
+        icon: <TeamOutlined/>,
         children: [
             {
                 label: "temp1",
@@ -49,60 +49,61 @@ const items:MenuItem[] = [
     {
         label: "菜单5",
         key: "/page5",
-        icon:  <FileOutlined />
+        icon: <FileOutlined/>
     },
 ]
-const MainMenu: React.FC =() => {
+const MainMenu: React.FC = () => {
     const location = useLocation()
+    const [openKeys, setOpenKeys] = useState<string[]>([firstOpenKey]);
+    const [menuKey, setMenuKey] = useState(location.pathname);
+    const useNavigateTo = useNavigate()
     //window.location.pathname = location.pathname
     //设置展开项的初始值
     //拿着location.pathname 与items数组的每一项的children的key做对比，如果找到了相同，则将他的父亲的key作为openKeys的初始值
-    let firstOpenKey:string = "";
-    const findKey = (obj: {key: string}) => {
+    let firstOpenKey: string = "";
+    const findKey = (obj: { key: string }) => {
         return obj.key === location.pathname
     }
-    // for(let i = 0; i< items.length; i++) {
-    //     if(items[i].children && !!items[i]!.children?.length && items[i]!.children.find(findKey)){
-    //         firstOpenKey = items[i]!.key :string
-    //         break;
-    //     }
+    for (let i = 0; i < items.length; i++) {
+        if (items[i]?.children.find(findKey)) {
+            firstOpenKey = items[i].key
+            break;
+        }
 
-    // }
-    const [openKeys, setOpenKeys] = useState<string[]>([firstOpenKey]);
-    const [menuKey, setMenuKey ] = useState(location.pathname);
+    }
+
 
     // useEffect(()=>{
     //     setMenuKey( window.location.pathname)
     // },[
     //     window.location.pathname
     // ])
-    const useNavigateTo = useNavigate()
-    const handleMenuClick = (e:{key: string}) => {
+    const handleMenuClick = (e: { key: string }) => {
         useNavigateTo(e.key)
     }
-    const handleOpenChange = (keys:string[]) => {
+    const handleOpenChange = (keys: string[]) => {
         //keys: 记录了的当前展开的数组key
         //展开和回收某一项的时候，执行这个函数里面的代码
         //把数组修改为最后一项
-        if(keys?.length < 1 ) {
+        if (keys?.length < 1) {
             setOpenKeys(keys)
-        }else {
-            setOpenKeys([keys[keys.length-1]])
+        } else {
+            setOpenKeys([keys[keys.length - 1]])
         }
     }
-    const handleSelect = (e: {key: string}) => {
+    const handleSelect = (e: { key: string }) => {
         setMenuKey(e.key)
     }
-    return ( <Menu
+    return (<Menu
         theme="dark"
-    selectedKeys={[menuKey]}
-    mode="inline"
-    items={items}
-    onClick={handleMenuClick}
-    onOpenChange={handleOpenChange}
-    openKeys={openKeys}
-    onSelect={handleSelect}
-     MenuProps={{}}/>)
+        selectedKeys={[menuKey]}
+        mode="inline"
+        items={items}
+        onClick={handleMenuClick}
+        onOpenChange={handleOpenChange}
+        openKeys={openKeys}
+        onSelect={handleSelect}
+    />)
 }
 
 export default MainMenu
--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,8 +1,27 @@
-import React from "react"
+import React, {ChangeEvent, useState} from "react"
 import { Input, Space, Button } from "antd"
 import styles from "./login.module.scss"
 import  "./login.less"
 const Login:React.FC = () => {
+    const [name, setName ] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const [ captcha, setCaptcha ] = useState<string>("")
+    const handleChangeName  = (e:ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value)
+    }
+    const handleChangePassWord = (e:ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value)
+    }
+    const handleChangeCaptcha = (e:ChangeEvent<HTMLInputElement>) => {
+        setCaptcha(e.target.value)
+    }
+    const handleClick = (e:MouseEvent) => {
+        e.stopPropagation()
+        console.log(name,'name')
+        console.log(password,'password')
+        console.log(captcha,'captcha')
+
+    }
     return <div className={styles.loginPage}>
         <div className={styles.loginContainer}>
             <div className={styles.titleContainer}>
@@ -12,15 +31,15 @@ const Login:React.FC = () => {
             <div className={styles.loginForm +  " inputCss"}>
                 <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
                     <p className={styles.formTitle }>登录</p>
-                    <Input placeholder="请输入用户名"/>
-                    <Input.Password placeholder="请输入密码" />
+                    <Input placeholder="请输入用户名" onChange={handleChangeName}/>
+                    <Input.Password placeholder="请输入密码" onChange={handleChangePassWord}/>
                     <div className={styles.captchaBox}>
                         <Space>
-                            <Input placeholder="验证码" />
+                            <Input placeholder="验证码" onChange={handleChangeCaptcha}/>
                             <img src={"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwww.n63.com%2Fphotodir%2Fn63img%2F%3FN%3DX2hiJTI2ZGRXJTVFVyU1RVdsV2tXJTYwbFdmVyU1RXIlNUNXZWclNUIlMjYlMkIuZiUyNm9vbyUyN2hnbGNrJTVEJTVDJTI3YWVfZllxJTI3WWZhJTYwJTVCJTI3%26v%3D.jpg&refer=http%3A%2F%2Fwww.n63.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1671896140&t=8ccd62d828416629acec1d494bc124fa"} />
                         </Space>
                     </div>
-                    <Button type="primary" block>登录</Button>
+                    <Button type="primary" block onClick={handleClick}>登录</Button>
                 </Space>
             </div>
         </div>
